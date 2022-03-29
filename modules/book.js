e6b6a87fbf4cb47e05de87d8b1873f10acf9a7e2
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-03-29 13:06:13 +0430

    create dir modules

---
+++ modules/book.js
@@ -0,0 +1,7 @@
+export default class Book {
+  constructor(title, author, bookId) {
+    this.title = title;
+    this.author = author;
+    this.bookId = bookId;
+  }
+}
---
+++ modules/store.js
@@ -0,0 +1,29 @@
+// Store Classs: Hundles Storage
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(bookId) {
+    const books = Store.getBooks();
+    books.forEach((book, index) => {
+      if (book.bookId === bookId) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+export default { Store };
